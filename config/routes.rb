de4d38a5dc64c5bec602d3bd4394a707ade4d1ab
tko99999 <araitakao@arii-inc.com>
2020-08-09 01:21:39 +0900

    Google認証周り

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'index#index'
+  devise_for :users,
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: "users/registrations",
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :boards do
     resources :messages,:only => [:create,:destroy]
