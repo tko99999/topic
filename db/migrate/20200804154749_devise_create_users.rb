312f428df5d9120cad794a7a35626ac157e7ab50
tko99999 <araitakao@arii-inc.com>
2020-08-10 12:27:59 +0900

    google認証周り

--- db/migrate/20200804154749_devise_create_users.rb
+++ db/migrate/20200804154749_devise_create_users.rb
@@ -41,6 +41,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
 
-      
+    t.string :nickname
   end
 end
